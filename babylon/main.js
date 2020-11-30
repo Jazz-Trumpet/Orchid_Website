138e5ad50eecb3a4af25cfc03cfc89296f639267
johnduong <jod033@ucsd.edu>
2020-11-29 18:23:46 -0800

    test

--- babylon/main.js
+++ babylon/main.js
@@ -82,52 +82,52 @@ var createScene = function () {
     ceiling.material = ceilingMaterial;
 
     /******************** Create Plane For Wall 1 (front) *********************/
-    var wall1 = BABYLON.MeshBuilder.CreatePlane("wall1", {height:10, width: 40, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
-    var wallMaterial = new BABYLON.StandardMaterial("wallColor", scene);
+    // var wall1 = BABYLON.MeshBuilder.CreatePlane("wall1", {height:10, width: 40, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
+    // var wallMaterial = new BABYLON.StandardMaterial("wallColor", scene);
     
-    //Postioning plane to the middle of the room
-    wall1.position = new BABYLON.Vector3(0, 0, -1);
-    wallMaterial.diffuseColor = new BABYLON.Color3(1,0,1);
+    // //Postioning plane to the middle of the room
+    // wall1.position = new BABYLON.Vector3(0, 0, -1);
+    // wallMaterial.diffuseColor = new BABYLON.Color3(1,0,1);
 
-    //Rotating plane to fit in wall
-    //ceiling.rotation.x = Math.PI / 2;
-    wall1.material = wallMaterial;
+    // //Rotating plane to fit in wall
+    // //ceiling.rotation.x = Math.PI / 2;
+    // wall1.material = wallMaterial;
 
-    /******************** Create Plane For Wall 2 (left) *********************/
-    var wall2 = BABYLON.MeshBuilder.CreatePlane("wall2", {height:10, width: 40, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
-    var wallMaterial = new BABYLON.StandardMaterial("wallColor", scene);
+    // /******************** Create Plane For Wall 2 (left) *********************/
+    // var wall2 = BABYLON.MeshBuilder.CreatePlane("wall2", {height:10, width: 40, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
+    // var wallMaterial = new BABYLON.StandardMaterial("wallColor", scene);
     
-    //Postioning plane to the middle of the room
-    wall2.position = new BABYLON.Vector3(-19.5, 0, -20);
-    wallMaterial.diffuseColor = new BABYLON.Color3(1,0,1);
+    // //Postioning plane to the middle of the room
+    // wall2.position = new BABYLON.Vector3(-19.5, 0, -20);
+    // wallMaterial.diffuseColor = new BABYLON.Color3(1,0,1);
 
-    //Rotating plane to fit in wall
-    wall2.rotation.y = -(Math.PI / 2);
-    wall2.material = wallMaterial;
+    // //Rotating plane to fit in wall
+    // wall2.rotation.y = -(Math.PI / 2);
+    // wall2.material = wallMaterial;
 
-    /******************** Create Plane For Wall 3 (right) *********************/
-    var wall3 = BABYLON.MeshBuilder.CreatePlane("wall3", {height:10, width: 40, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
-    var wallMaterial = new BABYLON.StandardMaterial("wallColor", scene);
+    // /******************** Create Plane For Wall 3 (right) *********************/
+    // var wall3 = BABYLON.MeshBuilder.CreatePlane("wall3", {height:10, width: 40, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
+    // var wallMaterial = new BABYLON.StandardMaterial("wallColor", scene);
     
-    //Postioning plane to the middle of the room
-    wall3.position = new BABYLON.Vector3(19.5, 0, -20);
-    wallMaterial.diffuseColor = new BABYLON.Color3(1,0,1);
+    // //Postioning plane to the middle of the room
+    // wall3.position = new BABYLON.Vector3(19.5, 0, -20);
+    // wallMaterial.diffuseColor = new BABYLON.Color3(1,0,1);
 
-    //Rotating plane to fit in wall
-    wall3.rotation.y = (Math.PI / 2);
-    wall3.material = wallMaterial;
+    // //Rotating plane to fit in wall
+    // wall3.rotation.y = (Math.PI / 2);
+    // wall3.material = wallMaterial;
 
-     /******************** Create Plane For Wall 4 (back) *********************/
-     var wall4 = BABYLON.MeshBuilder.CreatePlane("wall3", {height:10, width: 40, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
-     var wallMaterial = new BABYLON.StandardMaterial("wallColor", scene);
+    //  /******************** Create Plane For Wall 4 (back) *********************/
+    //  var wall4 = BABYLON.MeshBuilder.CreatePlane("wall3", {height:10, width: 40, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
+    //  var wallMaterial = new BABYLON.StandardMaterial("wallColor", scene);
      
-     //Postioning plane to the middle of the room
-     wall4.position = new BABYLON.Vector3(0, 0, -39.5);
-     wallMaterial.diffuseColor = new BABYLON.Color3(1,0,1);
+    //  //Postioning plane to the middle of the room
+    //  wall4.position = new BABYLON.Vector3(0, 0, -39.5);
+    //  wallMaterial.diffuseColor = new BABYLON.Color3(1,0,1);
  
-     //Rotating plane to fit in wall
-     wall4.rotation.y = -(Math.PI);
-     wall4.material = wallMaterial;
+    //  //Rotating plane to fit in wall
+    //  wall4.rotation.y = -(Math.PI);
+    //  wall4.material = wallMaterial;
 
     //Set gravity for the scene (G force like, on Y-axis)
     scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
